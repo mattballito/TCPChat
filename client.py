5e7a012c7ddc1411bba1413ce50bc16728bdf5c3
parthiv99 <parthadesai1999@gmail.com>
2023-05-20 01:38:22 -0800

    Successful task - Invite, Accept and get all inputs..

--- client.py
+++ client.py
@@ -3,74 +3,85 @@ import threading
 import json
 import hashlib
 
-userName = input("Enter your username: ")
-passWord = input("Enter your password: ")
+host = '127.0.0.1'
+port = 55555
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect((host, port))
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
 
 private_chat_active = False
 private_chat_partner = ''
 
 def receive():
-    global private_chat_active
+    global private_chat_active, private_chat_partner
     while True:
         try:
             message = client.recv(1024).decode('ascii')
+
             if message == 'NICK':
-                client.send(userName.encode('ascii'))
-                hashed_password = hashlib.sha256(passWord.encode()).hexdigest()
+                client.send(username.encode('ascii'))
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
                 client.send(hashed_password.encode('ascii'))
-            elif message == "USERLIST":
+
+            elif message == 'USERLIST':
                 message = client.recv(1024).decode('ascii')
-                if message == "LISTDATA":
+                if message == 'LISTDATA':
                     received_data = client.recv(1024)
                     deserialized_data = json.loads(received_data.decode())
 
                     if len(deserialized_data) > 0:
                         print("Here are the online users:\n", deserialized_data)
                     else:
-                        print("No other users are online at the moment\n")
+                        print("No other users are online at the moment.\n")
+
             elif message.startswith('INVITE '):
                 inviter = message.split(' ')[1]
                 print(f'You have received an invitation from {inviter}!\nType "ACCEPT {inviter}" to accept the invitation.')
-            elif message.startswith('ACCEPTED '):
-                recipient = message.split(' ')[1]
-                print(f'Your invitation to {recipient} has been accepted. You can now start a private chat session.')
-                private_chat_partner = recipient
-                private_chat_active = True
-            elif message.startswith('CHAT '):
-                sender = message.split(' ')[1]
-                chat_message = message.split(' ', 2)[2]
-                print(f'{sender}: {chat_message}')
-            elif message == 'CHATSTART':
-                print("Private chat started. You can now communicate privately.\n")
-                private_chat_active = True
+
+
+
             elif message == 'CHATEND':
-                print("Private chat ended. You can now communicate publicly.\n")
+                print("Private chat ended. You can now communicate publicly.")
                 private_chat_active = False
+                private_chat_partner = ''
+
             else:
                 print(message)
-                if message == 'RESTART':
-                    print("Invalid login. Please restart the client!")
-                    client.close()
+
+            if message == 'RESTART':
+                print("Invalid login. Please restart the client!")
+                client.close()
+
         except:
             print("An error occurred!")
             client.close()
             break
 
 def write():
-    global private_chat_partner
+    global private_chat_active, private_chat_partner
     while True:
-        message = input("")
-        if message.startswith('ENDCHAT'):
-            client.send('ENDCHAT'.encode('ascii'))
-            private_chat_partner = ''
-        elif private_chat_active and private_chat_partner and message:
-            client.send(f'CHAT {private_chat_partner} {message}'.encode('ascii'))
-        elif message:
-            client.send(f'{userName}: {message}'.encode('ascii'))
+        message = input()
+        if message.startswith('INVITE'):
+            recipient = message.split(' ')[1]
+            client.send(f'INVITE {recipient}'.encode('ascii'))
+
+        elif message.startswith('ACCEPT'):
+            inviter = message.split(' ')[1]
+            client.send(f'ACCEPT {inviter}'.encode('ascii'))
+
+        elif private_chat_active:
+            if message.startswith('ENDCHAT'):
+                client.send('ENDCHAT'.encode('ascii'))
+                private_chat_active = False
+                private_chat_partner = ''
+            elif message:
+                client.send(f'CHAT {private_chat_partner} {message}'.encode('ascii'))
+
         else:
-            print("Invalid command or message. Please try again.\n")
+            client.send(message.encode('ascii'))
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -16,7 +16,7 @@ nicknames = []
 def read_user_registry(file_path):
     user_registry = {}
     with open(file_path, 'r') as file:
-     for line in file:
+        for line in file:
             user, hashed_password = line.strip().split(':')
             user_registry[user] = hashed_password
     return user_registry
@@ -26,12 +26,12 @@ onlineUsers = {}
 
 private_chats = {}
 
-def getOnlineUsers(clientIdentity):
-    onlineList = []
+def get_online_users(client_identity):
+    online_list = []
     for key in onlineUsers.keys():
-        if (onlineUsers[key] != clientIdentity):
-            onlineList.append(key)
-    serialized_list = json.dumps(onlineList).encode()
+        if onlineUsers[key] != client_identity:
+            online_list.append(key)
+    serialized_list = json.dumps(online_list).encode()
     return serialized_list
 
 def broadcast(message, sender):
@@ -48,30 +48,42 @@ def handle_private_chat(client, recipient):
             break
 
 def handle(client):
+    in_private_chat = False
+    private_chat_partner = None
     while True:
         try:
             message = client.recv(1024)
-            stringMessage = message.decode()
-            listMessage = stringMessage.split()
+            string_message = message.decode()
+            list_message = string_message.split()
 
-            if (listMessage[1] == "ONLINE"):
-                onlineList = getOnlineUsers(client)
-                client.send(onlineList)
-            elif (listMessage[1] == "INVITE"):
-                recipient = listMessage[2]
+            if list_message[0] == 'ONLINE':
+                online_list = get_online_users(client)
+                client.send(online_list)
+
+            elif list_message[0] == 'INVITE':
+                recipient = list_message[1]
                 if recipient in onlineUsers:
                     onlineUsers[recipient].send(f'INVITE {nicknames[sessionUsers.index(client)]}'.encode('ascii'))
-            elif (listMessage[1] == "ACCEPT"):
-                sender = listMessage[2]
+
+            elif list_message[0] == 'ACCEPT':
+                sender = list_message[1]
                 if sender in onlineUsers:
                     private_chats[client] = onlineUsers[sender]
                     private_chats[onlineUsers[sender]] = client
-                    thread = threading.Thread(target=handle_private_chat, args=(client, onlineUsers[sender]))
-                    thread.start()
-                    thread = threading.Thread(target=handle_private_chat, args=(onlineUsers[sender], client))
-                    thread.start()
+                    in_private_chat = True
+                    private_chat_partner = onlineUsers[sender]
+
+            elif list_message[0] == 'ENDCHAT':
+                in_private_chat = False
+                private_chat_partner = None
+
+            elif in_private_chat:
+                # If in a private chat, send the message to the other chat participant.
+                private_chat_partner.send(message)
+
             else:
                 broadcast(message, client)
+
         except:
             index = sessionUsers.index(client)
             sessionUsers.remove(client)
@@ -82,6 +94,7 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
+
 def receive():
     while True:
         client, address = server.accept()
@@ -90,30 +103,29 @@ def receive():
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         hashed_password = client.recv(1024).decode('ascii')
-        if (str(nickname) in userRegistry):
-            if (userRegistry[f'{nickname}'] == hashed_password):
-                print(f'{nickname} is now online in the server!')
-                onlineUsers[nickname] = client
-                print("SERVER: this is dict:", onlineUsers)
-                nicknames.append(nickname)
 
-                client.send('USERLIST'.encode('ascii'))
-                client.send('LISTDATA'.encode('ascii'))
+        if nickname in userRegistry and userRegistry[nickname] == hashed_password:
+            print(f'{nickname} is now online in the server!')
+            onlineUsers[nickname] = client
+            nicknames.append(nickname)
 
-                serialized_list = getOnlineUsers(client)
-                client.send(serialized_list)
+            client.send('USERLIST'.encode('ascii'))
+            client.send('LISTDATA'.encode('ascii'))
 
-                sessionUsers.append(client)
-                broadcast(f'{nickname} joined the chat!'.encode('ascii'), client) #Add client as sender
-                client.send('Connected to the server!\n\tIf you would like to invite, type "INVITE"'.encode('ascii'))
-                thread = threading.Thread(target=handle, args=(client,))
-                thread.start()
-            else:
-                print(f'{nickname} exists, but password is incorrect')
-                client.send('RESTART'.encode('ascii'))
+            serialized_list = get_online_users(client)
+            client.send(serialized_list)
+
+            sessionUsers.append(client)
+            broadcast(f'{nickname} joined the chat!'.encode('ascii'), client)
+            client.send('Connected to the server!\n\tIf you would like to invite, type "INVITE"'.encode('ascii'))
+
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+
         else:
-            print("No such user exists in registry.")
+            print(f'{nickname} exists, but the password is incorrect')
             client.send('RESTART'.encode('ascii'))
+
 
 print("Server is listening..")
 receive()
